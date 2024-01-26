a34d59c8b5906739befcc3c9dc8349a91df25c0f
boubacargadio <boubacar.gadio@le-campus-numerique.fr>
2024-01-26 14:56:17 +0100

    test

--- game_board/src/Main.java
+++ game_board/src/Main.java
@@ -1,5 +1,10 @@
+import fr.lecampusnumerique.cda2025.javaalgo.boardgames.PlayerTurns.PlayerTurn;
+import fr.lecampusnumerique.cda2025.javaalgo.boardgames.PlayerTurns.PlayerTurns;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        PlayerTurns playerTurns = new PlayerTurn();
+        playerTurns.getPosition();
     }
 }
