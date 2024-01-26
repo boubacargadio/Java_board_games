f04c83b86c2aa14c25affc1d3838d6f4309051b3
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-26 15:38:47 +0100

    Cell: remove extra cells and only keep one object Cell

--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/cell/Connect4Cell.java
+++
@@ -1,9 +0,0 @@
-package fr.lecampusnumerique.cda2025.javaalgo.boardgames.cell;
-
-import fr.lecampusnumerique.cda2025.javaalgo.boardgames.symbols.Connect4Symbol;
-
-public class Connect4Cell extends AbstractCell {
-    protected Connect4Cell() {
-        super(Connect4Symbol.EMPTY);
-    }
-}
