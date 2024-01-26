72bda1a4481fb943d9e1f0aa51e3b04452e4c786
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-26 13:43:31 +0100

    Boards: make children boards and set col and rows

---
+++ game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/board/TicTacToeBoard.java
@@ -0,0 +1,8 @@
+package fr.lecampusnumerique.cda2025.javaalgo.boardgames.board;
+
+public class TicTacToeBoard extends AbstractBoard {
+
+    public TicTacToeBoard() {
+        super(3, 3);
+    }
+}
