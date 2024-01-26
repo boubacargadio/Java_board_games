843e893cec1d3499ac6ee9453707781e33fd8306
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-26 09:21:29 +0100

    BoardGames: Add selectGame method

--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/games/Games.java
+++ game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/games/Games.java
@@ -1,6 +1,7 @@
 package fr.lecampusnumerique.cda2025.javaalgo.boardgames.games;
 
 public enum Games {
+    DEFAULT("Undefined"),
     TICTACTOE("Tic Tac Toe"),
     CONNECT4("Connect 4"),
     GOMOKU("Gomoku");
@@ -8,6 +9,8 @@ public enum Games {
     private String name;
 
     public String getName() { return name;}
-    
-    Games(String name) { this.name = name;}
+
+    Games(String name) {
+        this.name = name;
+    }
 }
