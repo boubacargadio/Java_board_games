07ba472cbe6816e514a5aa69758c8b776deb991f
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-26 15:39:09 +0100

    Board: remove all boards to keep only one board

--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/board/Connect4Board.java
+++
@@ -1,9 +0,0 @@
-package fr.lecampusnumerique.cda2025.javaalgo.boardgames.board;
-
-public class Connect4Board extends AbstractBoard {
-
-
-    public Connect4Board() {
-        super(6, 7);
-    }
-}
--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/board/TicTacToeBoard.java
+++
@@ -1,8 +0,0 @@
-package fr.lecampusnumerique.cda2025.javaalgo.boardgames.board;
-
-public class TicTacToeBoard extends AbstractBoard {
-
-    public TicTacToeBoard() {
-        super(3, 3);
-    }
-}
