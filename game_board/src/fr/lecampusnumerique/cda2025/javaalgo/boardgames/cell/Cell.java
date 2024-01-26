269af1787d00acbc4559a32e5a4b33840278488c
anaisbonin <anais.bonin@le-campus-numerique.fr>
2024-01-26 13:54:16 +0100

    Cells : uptdates Cells to display correct default Empty

--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/cell/Cell.java
+++ game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/cell/Cell.java
@@ -1,7 +1,6 @@
 package fr.lecampusnumerique.cda2025.javaalgo.boardgames.cell;
 
 public interface Cell {
-    //Symbol getSymbol();
     boolean isValueCompatible();
     boolean isAvailable();
 }
---
+++ game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/cell/GomokuCell.java
@@ -0,0 +1,9 @@
+package fr.lecampusnumerique.cda2025.javaalgo.boardgames.cell;
+
+import fr.lecampusnumerique.cda2025.javaalgo.boardgames.symbols.GomokuSymbol;
+
+public class GomokuCell extends AbstractCell {
+    protected GomokuCell() {
+        super(GomokuSymbol.EMPTY);
+    }
+}
