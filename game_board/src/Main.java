c2dc22cbc206179e26de728a6de5da4dfad0d5ef
Xavier Favier <xaxou13@gmail.com>
2024-01-29 11:11:51 +0100

    Suggestions Anais

--- game_board/src/Main.java
+++ game_board/src/Main.java
@@ -3,8 +3,6 @@ import fr.lecampusnumerique.cda2025.javaalgo.boardgames.PlayerTurns.PlayerTurns;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        PlayerTurns playerTurns = new PlayerTurn();
-        playerTurns.getPosition();
+
     }
 }
--- game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/games/GameClass.java
+++ game_board/src/fr/lecampusnumerique/cda2025/javaalgo/boardgames/games/GameClass.java
@@ -1,7 +1,6 @@
 package fr.lecampusnumerique.cda2025.javaalgo.boardgames.games;
 
 public class GameClass implements Game {
-    //XF.
     private boolean isOver_util(boolean same, String curStr) {
         if(same && curStr != Cell.EMPTY_STR) {
             if(curStr.equals(player1.getRepresentation())) {
@@ -16,66 +15,72 @@ public class GameClass implements Game {
         }
         return false;
     }
-    //
+
     protected int checkSize = 0;
     void setCheckSize(int input) {
         checkSize = input;
     }
 
+    private boolean lineCheck() {
+        boolean same = true;
+        String curStr = myTable[a][b].getRepresentation();
+        if(b+checkSize <= myTable[a].length) {
+            for(int c=0; c < checkSize; c++) {
+                if(myTable[a][b+c].getRepresentation() != curStr) {
+                    same = false;
+                }
+            }
+            if(isOver_util(same, curStr)) {return true;}
+        }
+    }
+    private boolean columnCheck() {
+        boolean same = true;
+        String curStr = myTable[a][b].getRepresentation();
+        if(a+checkSize <= myTable.length) {
+            for (int c=0; c < checkSize; c++) {
+                if (myTable[a+c][b].getRepresentation() != curStr) {
+                    same = false;
+                }
+            }
+            if(isOver_util(same, curStr)) {return true;}
+        }
+    }
+    private boolean diagonal1Check() {
+        boolean same = true;
+        String curStr = myTable[a][b].getRepresentation();
+        if(b+checkSize <= myTable[a].length &&
+                a+checkSize <= myTable.length) {
+
+            for(int c=0; c < checkSize; c++) {
+                if(myTable[a+c][b+c].getRepresentation() != curStr) {
+                    same = false;
+                }
+            }
+            if(isOver_util(same, curStr)) {return true;}
+        }
+    }
+    private boolean diagonal2Check() {
+        boolean same = true;
+        String curStr = myTable[a][b].getRepresentation();
+        if(b+checkSize <= myTable[a].length &&
+                a-checkSize >= -1) {
+
+            for(int c=0; c < checkSize; c++) {
+                if(myTable[a-c][b+c].getRepresentation() != curStr) {
+                    same = false;
+                }
+            }
+            if(isOver_util(same, curStr)) {return true;}
+        }
+    }
+
     protected boolean isOver() {
         for(int a=0; a < myTable.length; a++) {
             for(int b=0; b < myTable.length; b++) { //for every cell
-                //line
-                boolean same = true;
-                String curStr = myTable[a][b].getRepresentation();
-                if(b+checkSize <= myTable[a].length) {
-                    for(int c=0; c < checkSize; c++) {
-                        if(myTable[a][b+c].getRepresentation() != curStr) {
-                            same = false;
-                        }
-                    }
-                    if(isOver_util(same, curStr)) {return true;}
-                }
-
-                //column
-                same = true;
-                curStr = myTable[a][b].getRepresentation();
-                if(a+checkSize <= myTable.length) {
-                    for (int c=0; c < checkSize; c++) {
-                        if (myTable[a+c][b].getRepresentation() != curStr) {
-                            same = false;
-                        }
-                    }
-                    if(isOver_util(same, curStr)) {return true;}
-                }
-
-                //diagonal1
-                same = true;
-                curStr = myTable[a][b].getRepresentation();
-                if(b+checkSize <= myTable[a].length &&
-                        a+checkSize <= myTable.length) {
-
-                    for(int c=0; c < checkSize; c++) {
-                        if(myTable[a+c][b+c].getRepresentation() != curStr) {
-                            same = false;
-                        }
-                    }
-                    if(isOver_util(same, curStr)) {return true;}
-                }
-
-                //diagonal2
-                same = true;
-                curStr = myTable[a][b].getRepresentation();
-                if(b+checkSize <= myTable[a].length &&
-                        a-checkSize >= -1) {
-
-                    for(int c=0; c < checkSize; c++) {
-                        if(myTable[a-c][b+c].getRepresentation() != curStr) {
-                            same = false;
-                        }
-                    }
-                    if(isOver_util(same, curStr)) {return true;}
-                }
+                if(lineCheck()) {return true;}
+                if(columnCheck()) {return true;}
+                if(diagonal1Check()) {return true;}
+                if(diagonal2Check()) {return true;}
             }
         }
 
